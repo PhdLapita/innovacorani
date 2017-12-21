679b7ba7813c6406ec0b7e8da8b36c13cb77d105
PhdLapita <glapav@uni.pe>
2017-12-21 12:54:00 -0500

    comentario17

--- index.php
+++ index.php
@@ -117,10 +117,10 @@
 
 							<div class="12u 12u(narrow)">
 								<div class="row">
-									<div class="4u"><a href="#" class="image fit"><img src="img/middle/1.jpg" alt="" /></a></div>
-									<div class="4u"><a href="#" class="image fit"><img src="img/middle/2.jpg" alt="" /></a></div>
-									<div class="4u"><a href="#" class="image fit"><img src="img/middle/3.jpg" alt="" /></a></div>
-									<div class="4u"><a href="#" class="image fit"><img src="img/middle/4.jpg" alt="" /></a></div>
+									<div class="4u"><a href="#" class="image fit"><img src="img/middle/11.jpg" alt="" /></a></div>
+									<div class="4u"><a href="#" class="image fit"><img src="img/middle/22.jpg" alt="" /></a></div>
+									<div class="4u"><a href="#" class="image fit"><img src="img/middle/33.jpg" alt="" /></a></div>
+									<div class="4u"><a href="#" class="image fit"><img src="img/middle/44.jpg" alt="" /></a></div>
 									<div class="4u"><a href="#" class="image fit"><img src="img/middle/5.jpg" alt="" /></a></div>
 									<div class="4u"><a href="#" class="image fit"><img src="img/middle/6.jpg" alt="" /></a></div>
 								</div>
