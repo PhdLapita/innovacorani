09b0f80d32e8badd4e637aafd5a685ddee6bcf82
PhdLapita <glapav@uni.pe>
2017-12-21 11:04:29 -0500

    comentario9

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
 								<section>
 									<span class="feature-icon"><img class="icon" src="img/persona1.jpg"/></span>
 									<header>
-										<h3>Justin Trudeau</h3>
+										<h3>Alfonso Pilco</h3>
 									</header>
 									<p>Gravida dis placerat lectus ante vel nunc euismod est turpis sodales.
 									Diam tempor dui lacinia accumsan vivamus. Gravida dis placerat lectus
